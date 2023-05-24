cac524ffbfa50f7f6c0b40ed24b32e3c3f32516b
krasimiranova <krasy_nova@yahoo.com>
2023-05-24 10:37:33 +0200

    controllers, templates

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const hbs = require('express-handlebars').create({extname : '.hbs'});
 
+const homeController = require('./controllers/homeController');
+const errorController = require('./controllers/errorController');
+const catalogController = require('./controllers/catalogController');
+const createController = require('./controllers/createController');
+const defaultTitle = require('./middlewares/defaultTitle');
+
 const app = express();
 
 app.engine('.hbs', hbs.engine);
@@ -8,6 +14,15 @@ app.set('view engine', '.hbs');
 
 app.use(express.urlencoded({ extended : true }));
 app.use('/static', express.static('static')); 
+
+app.use(defaultTitle('SoftUni Accomodation'));
+
+app.use(homeController);
+app.use('/catalog', catalogController);
+app.use('/create', createController);
+
+
+app.all('*', errorController);
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
